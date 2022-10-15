5150d521df16f542b223388610c625ed15812a38
Josip Prpić <47066785+josippr@users.noreply.github.com>
2022-10-15 20:33:19 +0200

    Add files via upload

---
+++ Tic-tac-toe/Tic-tac-toe/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_tac_toe
+{
+    class Player
+    {
+        private string name;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
